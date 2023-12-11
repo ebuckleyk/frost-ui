326f5562a270e392a8057d3e4e2ecd4a7419ee24
Emmanuel K. Buckley <ebuckley2389@gmail.com>
2023-12-11 15:53:32 -0600

    chore: cleanup unused storybook files

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -7,7 +7,14 @@ export default create({
   brandImage: 'favicons/favicon-32x32.png',
   brandTarget: '_self',
 
-  //
-  colorPrimary: '#3675e2',
-  colorSecondary: '#f1f5f9',
+  // //
+  // colorPrimary: '#3675e2',
+  // colorSecondary: '#f1f5f9',
+
+  // // UI
+  // appBg: '#dce7fe',
+  // appContentBg: '#dce7fe',
+  // appPreviewBg: '#dce7fe',
+  // appBorderColor: '#dce7fe',
+  // appBorderRadius: 4,
 });
