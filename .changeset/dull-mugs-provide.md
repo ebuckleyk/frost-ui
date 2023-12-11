65f07efdab8124de90bea51c9bf68b213a55e761
Emmanuel K. Buckley <ebuckley2389@gmail.com>
2023-12-11 15:35:36 -0600

    added missing files

---
+++ .changeset/dull-mugs-provide.md
@@ -0,0 +1,5 @@
+---
+'@ebuckleyk/frost-ui': minor
+---
+
+Updated storybook components. Change opacity for buttons and made RichTextEditor Toolbar outline variant by default
---
+++ .storybook/viewports.ts
@@ -0,0 +1,75 @@
+// https://www.icwebdesign.co.uk/common-viewport-sizes
+export default {
+  iPhone12ProMax: {
+    name: 'iPhone 12 Pro Max',
+    type: 'mobile',
+    styles: {
+      width: '428px',
+      height: '926px',
+    },
+  },
+  iPhone12Pro: {
+    name: 'iPhone 12 Pro',
+    type: 'mobile',
+    styles: {
+      width: '390px',
+      height: '844px',
+    },
+  },
+  iPadPro: {
+    name: 'iPad Pro',
+    type: 'tablet',
+    styles: {
+      width: '1024px',
+      height: '1366px',
+    },
+  },
+  iPad3_4Generation: {
+    name: 'iPad 3/4 Gen',
+    type: 'tablet',
+    styles: {
+      width: '768px',
+      height: '1024px',
+    },
+  },
+  'samsunggalaxys9+': {
+    name: 'Samsung Galaxy S9+',
+    type: 'mobile',
+    styles: {
+      width: '360px',
+      height: '853px',
+    },
+  },
+  'samsunggalaxys8+': {
+    name: 'Samsung Galaxy S8+',
+    type: 'mobile',
+    styles: {
+      width: '360px',
+      height: '740px',
+    },
+  },
+  samsunggalaxynote9: {
+    name: 'Samsung Galaxy Note 9',
+    type: 'mobile',
+    styles: {
+      width: '360px',
+      height: '740px',
+    },
+  },
+  googlepixel3XL: {
+    name: 'Google Pixel 3 XL',
+    type: 'mobile',
+    styles: {
+      width: '412px',
+      height: '847px',
+    },
+  },
+  samsunggalaxytab10: {
+    name: 'Samsung Galaxy Tab 10',
+    type: 'tablet',
+    styles: {
+      width: '800px',
+      height: '1280px',
+    },
+  },
+};
