f33df23c66dd79fe1a9bf3c6d33464ef30adcc5b
Emmanuel K. Buckley <ebuckley2389@gmail.com>
2024-02-12 22:52:41 -0600

    reconfigure library to support tree-shaking

--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,12 @@ import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
-import bundleSize from 'rollup-plugin-bundle-size';
 import copy from 'rollup-plugin-copy';
 import { dts } from 'rollup-plugin-dts';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
+//import bundleSize from 'rollup-plugin-bundle-size';
+import sizes from 'rollup-plugin-sizes';
 import { visualizer } from 'rollup-plugin-visualizer';
 import tailwindcss from 'tailwindcss';
 
@@ -23,15 +24,26 @@ const config = {
   input: 'src/index.ts',
   external: ['react', 'react-dom'],
   output: [
+    // {
+    //   // file: packageJson.main,
+    //   dir: 'dist/cjs',
+    //   format: 'cjs',
+    //   sourcemap: true,
+    //   preserveModules: true,
+    // },
     {
-      file: packageJson.main,
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      file: packageJson.module,
+      // file: packageJson.module,
+      dir: 'dist',
       format: 'esm',
       sourcemap: true,
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+      entryFileNames: (chunkInfo) => {
+        if (chunkInfo.name.includes('node_modules')) {
+          return chunkInfo.name.replace('node_modules', 'external') + '.js';
+        }
+        return '[name].js';
+      },
     },
   ],
   plugins: [
@@ -55,13 +67,11 @@ const config = {
       plugins: [tailwindcss(tailwindConfig)],
     }),
     copy({
-      targets: [
-        { src: './tailwind.config.js', dest: 'dist/configs/tailwindcss/' },
-        { src: './src/styles/theme-preset.js', dest: 'dist/presets/', rename: 'theme.js' },
-      ],
+      targets: [{ src: './src/styles/theme-preset.js', dest: 'dist/presets/', rename: 'theme.js' }],
     }),
     terser(),
-    bundleSize(),
+    //bundleSize(),
+    sizes(),
     visualizer(),
   ],
   onwarn(warning, warn) {
@@ -74,7 +84,7 @@ const config = {
  * @type {import('rollup').RollupOptions}
  */
 const typesConfig = {
-  input: 'dist/esm/types/index.d.ts',
+  input: 'dist/types/index.d.ts',
   output: [{ file: 'dist/index.d.ts', format: 'esm' }],
   plugins: [dts()],
   // https://stackoverflow.com/questions/71848226/creating-react-library-with-rollup-js-i-get-error-null-reading-usestate/7260405
