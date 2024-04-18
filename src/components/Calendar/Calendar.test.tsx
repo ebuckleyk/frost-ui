cc0f8db3aca63cb8b99c6376d65aa5ee35e0baf2
Emmanuel K. Buckley <ebuckley2389@gmail.com>
2024-04-17 23:16:45 -0500

    fix build

--- src/components/Calendar/Calendar.test.tsx
+++ src/components/Calendar/Calendar.test.tsx
@@ -4,7 +4,7 @@ import { render } from '@testing-library/react';
 import { Calendar } from './Calendar';
 
 const Component = () => {
-  const [date, setDate] = React.useState<Date | undefined>(new Date());
+  const [date, setDate] = React.useState<Date | undefined>(new Date(2024, 4, 16, 23, 23, 23, 30));
   return <Calendar mode="single" selected={date} onSelect={setDate} className="rounded-md border shadow" />;
 };
 
