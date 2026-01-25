e7a022f12723e467249f8fe2884d038df8b6eabc
Emmanuel K. Buckley <ebuckley2389@gmail.com>
2026-01-24 20:00:50 -0600

    fixed exports of new components

---
+++ .changeset/gorgeous-oranges-confess.md
@@ -0,0 +1,5 @@
+---
+'@ebuckleyk/frost-ui': minor
+---
+
+Fix exports of new components
--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -5,8 +5,9 @@ import { useMemo } from 'react';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
-import { Label } from '@/components/Label';
-import { Separator } from '@/components/Separator';
+
+import { Label } from '../Label';
+import { Separator } from '../Separator';
 
 function FieldSet({ className, ...props }: React.ComponentProps<'fieldset'>) {
   return (
